124e619b7f4415d2e147395f2ba0068b793ea93d
martendo <51766598+martendo@users.noreply.github.com>
2024-01-14 23:21:16 -0500

    Implement response verification

--- script.js
+++ script.js
@@ -81,25 +81,7 @@ window.addEventListener("resize", () => {
 window.addEventListener("load", () => {
 	updateCanvasSize();
 	updateCanvas();
-	setTimeout(() => {
-		addEmail(
-			"Welcome!",
-			"Venkat Kapoor",
-			"Director of Mars Operations at NASA",
-			"Hello!\n\nIt is my pleasure to welcome you to the team. This of NASA's most ambitious projects ever. I'm sure you are very aware of our situation with our dear friend Mark Watney. It is our duty to ensure he makes it back to Earth safe and healthy. Being stranded on Mars alone is a serious matter and we are doing everything in our power to get him home.\n\nCommunication among the team will take place over email. Honestly, we don't have the time or energy to do it any other way. You can respond to us with the buttons and text boxes at the bottom of the emails that warrant a response.\n\nThank you for taking on this huge challenge. By the way, you might want to refill your cup of coffee.",
-			0,
-			[["I got coffee!", () => setTimeout(() => {
-				addEmail(
-					"Let's get started",
-					"Venkat Kapoor",
-					"Director of Mars Operations at NASA",
-					"Hi again.\n\nCoffee's great. That fuel will be important. You simply can't work without that energy.\n\nEmergency funding from Congress basically means we can pay you for all the overtime in the world. But don't overwork yourself. You'll need rest eventually. Once you've done enough for the day, clock out. Take a break. You'll need it.\n\nMedia wants the latest position of Watney. Do you have it?",
-					1,
-					[["Position"]]
-				);
-			}, 500)]]
-		);
-	}, 3000);
+	CHECKPOINTS.intro();
 });
 
 let timeHours = 0;
@@ -142,9 +124,9 @@ function openEmail(fromDetails, message, responseType, responses) {
 	} else {
 		const table = document.createElement("table");
 		const inputs = [];
-		for (const response of responses) {
+		for (let i = 1; i < responses.length; i++) {
 			const responseInput = document.createElement("input");
-			responseInput.placeholder = response[0];
+			responseInput.placeholder = responses[i][0];
 			table.insertRow().insertCell().appendChild(responseInput);
 			inputs.push(responseInput);
 		}
@@ -152,9 +134,15 @@ function openEmail(fromDetails, message, responseType, responses) {
 		submitButton.textContent = "Submit";
 		submitButton.className = "submitbutton";
 		submitButton.addEventListener("click", () => {
-			for (const input of inputs) {
-				console.log(input.value);
+			for (let i = 0; i < inputs.length; i++) {
+				if (!responses[i + 1][1](inputs[i].value)) {
+					// Fail
+					CHECKPOINTS[responses[0][1]]();
+					return;
+				}
 			}
+			// Pass
+			CHECKPOINTS[responses[0][0]]();
 		});
 		const submitCell = table.insertRow().insertCell();
 		submitCell.colSpan = 2;
@@ -177,16 +165,56 @@ function addEmail(subject, from, fromDetails, message, responseType, responses) 
 	});
 }
 
+const CHECKPOINTS = {
+	"intro": () => {
+		setTimeout(() => {
+			addEmail(
+				"Welcome!",
+				"Venkat Kapoor",
+				"Director of Mars Operations at NASA",
+				"Hello!\n\nIt is my pleasure to welcome you to the team. This of NASA's most ambitious projects ever. I'm sure you are very aware of our situation with our dear friend Mark Watney. It is our duty to ensure he makes it back to Earth safe and healthy. Being stranded on Mars alone is a serious matter and we are doing everything in our power to get him home.\n\nCommunication among the team will take place over email. Honestly, we don't have the time or energy to do it any other way. You can respond to us with the buttons and text boxes at the bottom of the emails that warrant a response.\n\nThank you for taking on this huge challenge. By the way, you might want to refill your cup of coffee.",
+				0,
+				[["I got coffee!", () => CHECKPOINTS.pos1()]]
+			);
+		}, 3000);
+	},
+	"pos1": () => {
+		setTimeout(() => {
+			addEmail(
+				"Let's get started",
+				"Venkat Kapoor",
+				"Director of Mars Operations at NASA",
+				"Hi again.\n\nCoffee's great. That fuel will be important. You simply can't work without that energy.\n\nEmergency funding from Congress basically means we can pay you for all the overtime in the world. But don't overwork yourself. You'll need rest eventually. Once you've done enough for the day, clock out. Take a break. You'll need it.\n\nMedia wants the latest position of Watney. Do you have it?",
+				1,
+				[
+					["pos1pass", "pos1fail"],
+					["Latitude", (res) => Math.abs(parseFloat(res) - WATNEY_LAT_1) < 1],
+					["Longitude", (res) => Math.abs(parseFloat(res) - WATNEY_LONG_1) < 1],
+				]
+			);
+		}, 1000);
+	},
+	"pos1pass": () => {
+		console.log("yay");
+	},
+	"pos1fail": () => {
+		console.log("boo");
+	},
+};
+
 const MAP_OFFSET_X = -28.3;
 const MAP_OFFSET_Y = 31.9;
 const MAP_WIDTH = 22.9 - MAP_OFFSET_X;
 const MAP_HEIGHT = -7.4 - MAP_OFFSET_Y;
 
+const WATNEY_LAT_1 = 21.367;
+const WATNEY_LONG_1 = -14.149;
+
 const mapCursorPosSpan = document.getElementById("mapcursorpos");
 
 document.getElementById("map").addEventListener("pointermove", (event) => {
 	const mapRect = event.currentTarget.getBoundingClientRect();
-	const x = (event.clientX - mapRect.left) / mapRect.width * MAP_WIDTH + MAP_OFFSET_X;
-	const y = (event.clientY - mapRect.top) / mapRect.height * MAP_HEIGHT + MAP_OFFSET_Y;
-	mapCursorPosSpan.textContent = `${x.toFixed(3)}, ${y.toFixed(3)}`;
+	const long = (event.clientX - mapRect.left) / mapRect.width * MAP_WIDTH + MAP_OFFSET_X;
+	const lat = (event.clientY - mapRect.top) / mapRect.height * MAP_HEIGHT + MAP_OFFSET_Y;
+	mapCursorPosSpan.textContent = `${lat.toFixed(3)}, ${long.toFixed(3)}`;
 });
